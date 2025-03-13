feec3205c54a3e996bc644ea2f04cd88542fb630
Neil Benedict Reyes <reyesneil211@gmail.com>
2025-03-13 19:54:56 +0800

    Added blog

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 const PORT = 3000;
@@ -17,6 +18,20 @@ app.get('/about', (req, res) => {
 app.get('/contact', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'contact.html'));
 });
+
+app.get('/blog', (req, res) => {
+    fs.readFile('./data/post.json', 'utf8', (err, data) => {
+        if (err) {
+            return res.status(500).send('Error reading posts file.');
+        }
+        
+        const posts = JSON.parse(data);
+        res.render('blog', { posts });
+    });
+});
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
